0f3272b4dfd18afb0e71d208e1943815f483e7ed
Sig Lange <sig.lange@gmail.com>
2015-03-12 15:16:02 -0700

    I fixed it

--- cask/prereq.go
+++ cask/prereq.go
@@ -1,15 +1,16 @@
 package main
-import (
-   "fmt"
-   "strings"
-	"gopkg.in/lxc/go-lxc.v2"
-)
-func CheckPrerequisites() error {
-   version := lxc.Version()
-   fmt.Println("lxc version", version)
 
-   if strings.HasPrefix(version, "1.1.") == false {
-      return fmt.Errorf("version requirement not met: need atleast 1.1.x, have %s", version)
-   }
-   return nil
+import (
+	"fmt"
+	"gopkg.in/lxc/go-lxc.v2"
+	"strings"
+)
+
+func CheckPrerequisites() error {
+	version := lxc.Version()
+
+	if strings.HasPrefix(version, "1.1.") == false {
+		return fmt.Errorf("version requirement not met: need atleast 1.1.x, have %s", version)
+	}
+	return nil
 }

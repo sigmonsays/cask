e726311ee3bb2495089cc9e80d56eef478f383a0
Sig Lange <sig.lange@gmail.com>
2015-03-11 17:34:38 -0700

    convenience function and gofmt

--- cask/util.go
+++ cask/util.go
@@ -1,26 +1,35 @@
 package main
+
 import (
-   "os"
-   "os/exec"
-   "github.com/termie/go-shutil"
+	"github.com/termie/go-shutil"
+	"os"
+	"os/exec"
 )
+
+func FileExists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		return false
+	}
+	return true
+}
+
 func CopyFile(src, dst string, mode int) error {
-   err := shutil.CopyFile(src, dst, false)
-   if err != nil {
-      return err
-   }
-   err = os.Chmod(dst, os.FileMode(mode))
-   if err != nil {
-      return err
-   }
-   return nil
+	err := shutil.CopyFile(src, dst, false)
+	if err != nil {
+		return err
+	}
+	err = os.Chmod(dst, os.FileMode(mode))
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func GetDefaultRuntime() string {
-   if out, err := exec.Command("lsb_release", "-cs").Output(); err == nil {
-      if len(out) > 0 {
-         return string(out)
-      }
-   }
-   return ""
+	if out, err := exec.Command("lsb_release", "-cs").Output(); err == nil {
+		if len(out) > 0 {
+			return string(out)
+		}
+	}
+	return ""
 }

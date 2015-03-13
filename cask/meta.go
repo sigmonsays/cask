82afd851277096e8dc93868b19cadd476b0b13e9
Sig Lange <sig.lange@gmail.com>
2015-03-13 09:04:49 -0700

    Failure is not an option. Everyone has to succeed.

--- cask/meta.go
+++ cask/meta.go
@@ -1,13 +1,28 @@
 package main
 
 type Meta struct {
-	Runtime string              `json:"runtime"`
-	Name    string              `json:"name"`
-	Config  map[string][]string `json:"config"`
-	Data    map[string]string   `json:"data"`
-	Build   BuildParams         `json:"build"`
+	// name of the image
+	Name string `json:"name"`
+
+	// version of the image
+	Version string `json:"version"`
+
+	// runtime this image should run with
+	Runtime string `json:"runtime"`
+
+	// holds configuration parameters such as lxc.*
+	Config map[string][]string `json:"config"`
+
+	// arbtrary data to be stored with image
+	Data map[string]string `json:"data"`
+
+	// build paramters for building the image
+	Build BuildParams `json:"build"`
 }
 type BuildParams struct {
+
+	// other images to include in this build
+	Images []string `json:"images"`
 
 	// glob patterns of files to exclude in the saved image
 	Exclude []string `json:"exclude"`
